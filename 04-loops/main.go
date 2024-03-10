be3520398cb2c75051fb4b41df027ee4cf9da1bc
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-03-10 20:53:29 +0100

    create loops in go

---
+++ 04-loops/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	index := 0
+
+	for index <= 10 {
+		//	fmt.Println(index)
+		index++
+	}
+
+	total := 0
+	for i := 1; i <= 10; i++ {
+		total += i
+	}
+	fmt.Printf("Total: %d\n", total)
+
+	var names = [3]string{"Go", "Java", "C++"}
+
+	for counter := 0; counter < len(names); {
+		fmt.Println(names[counter])
+		counter++
+	}
+
+}
